e46cc901b5dea63bbb2f4e7e1691a2c66d5e8fe5
Rafay Kalim <rafay.kalim@mail.utoronto.ca>
2020-11-28 20:58:34 -0500

    add structure code for order book

--- OrderBook/OrderBook.py
+++ OrderBook/OrderBook.py
@@ -1,14 +1,22 @@
 import pandas as pd
 import numpy as np
+import sys
+
+sys.path.append('../') 
+from MatchObject import Match.Match
 
 class OrderBook:
     def __init__(self):
-        self.orders = pd.DataFrame() # To be filled
+        self.orders = pd.DataFrame() # To be filled when orders are added
     
     def add_orders(self, orders):
         if isinstance(orders, pd.DataFrame):
             self.orders = self.orders.append(orders, ignore_index=True)
-
+        else:
+            self.orders
         return 1
     
-    def update_matches(self, matches)
+    def update_matches(self, matches):
+        # Take in list of Match Object
+        # 
+        pass
